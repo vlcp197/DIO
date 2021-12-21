987ee31cd679c480d65ad43fb34915ab6c931ce2
vlcp197 <vlcp197@gmail.com>
2021-12-21 02:33:48 -0300

    Contador (Introdução ao JavaScript)

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+const currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if(currentNumber >=0){
+        currentNumberWrapper.style.color = 'black';
+    }
+}
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';   
+    }
+}
